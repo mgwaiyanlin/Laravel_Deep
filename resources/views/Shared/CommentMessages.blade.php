82f49bad8a6b728fe2c26c3b752101b117ce79fb
mgwaiyanlin <waiyanlinn.netuser@gmail.com>
2024-10-05 07:20:17 +0630

    test again

--- resources/views/Shared/CommentMessages.blade.php
+++ resources/views/Shared/CommentMessages.blade.php
@@ -4,8 +4,7 @@
     <div class="w-100">
         <div class="d-flex justify-content-between">
             <h6 class="">Luigi</h6>
-            <small class="fs-6 fw-light text-muted"> 3 hour
-                ago</small>
+            <small class="fs-6 fw-light text-muted"> 3 hour ago</small>
         </div>
         <p class="fs-6 mt-3 fw-light">
             {{ $comment->content }}
